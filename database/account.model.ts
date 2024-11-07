76fc7af07a9d925f3c7daf3dc62a2bb7f852ff8e
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-11-07 13:00:44 +0530

    create account model

---
+++ database/account.model.ts
@@ -0,0 +1,26 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IAccount {
+  userId: Schema.Types.ObjectId;
+  name: string;
+  image?: string;
+  provider: "Google" | "Github" | "Credentials"[];
+  password?: string;
+  providerAccountId: string;
+}
+const AccountSchema = new Schema<IAccount>({
+  userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  name: { type: String, required: true },
+  image: { type: String },
+  password: String,
+  provider: {
+    type: String,
+    enum: ["Google", "Github", "Credentials"],
+    required: true,
+  },
+  providerAccountId: { type: String, required: true },
+});
+
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
+
+export default Account;

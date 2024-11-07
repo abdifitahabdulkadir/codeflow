4a43fabaec97f1e0855fa81ddd81c3ae66e63fdb
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-11-07 13:35:04 +0530

    create quesion model

---
+++ database/quesion.model.ts
@@ -0,0 +1,29 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IQuesion {
+  title: string;
+  content: string;
+  authorId: Schema.Types.ObjectId;
+  tags: string[];
+  views: number;
+  upVotes: number;
+  downVotes: number;
+  answers: number;
+}
+const QuesionSchema = new Schema<IQuesion>(
+  {
+    title: { type: String },
+    content: { type: String },
+    authorId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Quesion = models?.Quesion || model<IQuesion>("Quesion", QuesionSchema);
+
+export default Quesion;

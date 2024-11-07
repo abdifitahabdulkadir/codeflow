5f61a97db0b810b14228ea249c12cc56ae6b113f
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-11-07 20:04:27 +0530

    create vote model

---
+++ database/vote.model.ts
@@ -0,0 +1,37 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IVote {
+  author: Types.ObjectId;
+  contentId: Types.ObjectId;
+  contentType: "question" | "answer";
+  voteType: "upvote" | "downvote";
+}
+const VoteSchema = new Schema<IVote>(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    contentId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    contentType: {
+      type: String,
+      enum: ["question", "answer"],
+      reqiured: true,
+    },
+    voteType: {
+      type: String,
+      enum: ["upvote", "downvote"],
+      reqiured: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Vote = models?.Vote || model("Vote", VoteSchema);
+
+export default Vote;
